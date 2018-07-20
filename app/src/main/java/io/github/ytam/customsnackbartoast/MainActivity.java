ac3fe240593f039b4c95cbff75f27331411fbc10
ytam <tamyldrm@gmail.com>
2018-07-20 17:57:29 +0300

    Add toast lenght and toast icon

--- app/src/main/java/io/github/ytam/customsnackbartoast/MainActivity.java
+++ app/src/main/java/io/github/ytam/customsnackbartoast/MainActivity.java
@@ -2,6 +2,7 @@ package io.github.ytam.customsnackbartoast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -18,30 +19,30 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.btnSuccess) void clickSuccess(){
 
-        CustomToast.success(this, "This is success message");
+        CustomToast.success(this, "This is success message",Toast.LENGTH_SHORT);
 
     }
 
     @OnClick(R.id.btnError) void clickError(){
 
-        CustomToast.error(this, "This is error message");
+        CustomToast.error(this, "This is error message",Toast.LENGTH_LONG);
     }
 
     @OnClick(R.id.btnInfo) void clickInfo(){
 
-        CustomToast.info(this, "This is info message");
+        CustomToast.info(this, "This is info message",Toast.LENGTH_LONG);
 
     }
 
     @OnClick(R.id.btnWarning) void clickWarning(){
 
-        CustomToast.warning(this, "This is warning message");
+        CustomToast.warning(this, "This is warning message",Toast.LENGTH_LONG);
 
     }
 
     @OnClick(R.id.btnNormal) void clickNormal(){
 
-        CustomToast.normal(this, "This is normal message");
+        CustomToast.normal(this, "This is normal message",Toast.LENGTH_LONG);
 
     }
 }
--- customtoast/src/main/java/io/github/ytam/customtoast/CustomToast.java
+++ customtoast/src/main/java/io/github/ytam/customtoast/CustomToast.java
@@ -1,63 +1,102 @@
 package io.github.ytam.customtoast;
 
+import android.app.Activity;
+import android.app.Application;
 import android.content.Context;
-import android.graphics.Color;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class CustomToast {
+public class CustomToast extends Toast {
 
-    public static void success(Context context, String message){
+    /**
+     * Construct an empty Toast object.  You must call {@link #setView} before you
+     * can call {@link #show}.
+     *
+     * @param context The context to use.  Usually your {@link Application}
+     *                or {@link Activity} object.
+     */
 
-        Toast toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
+
+    public CustomToast(Context context) {
+        super(context);
+    }
+
+    public static Toast success(Context context,String message,int duration){
+
+        Toast toast = new Toast(context);
+        toast.setDuration(duration);
+        View layout = LayoutInflater.from(context).inflate(R.layout.toast_layout, null, false);
+        TextView l1 = (TextView) layout.findViewById(R.id.toast_text);
+        LinearLayout linearLayout=(LinearLayout) layout.findViewById(R.id.toast_layout);
+        ImageView img=(ImageView) layout.findViewById(R.id.toast_icon);
+        l1.setText(message);
+        linearLayout.setBackgroundResource(R.drawable.bg_success);
+        img.setImageResource(R.drawable.ic_done_black_24dp);
+        toast.setView(layout);
+        toast.show();
+        return toast;
+    }
+
+    public static Toast error(Context context,String message,int duration){
+
+        Toast toast = new Toast(context);
+        toast.setDuration(duration);
+        View layout = LayoutInflater.from(context).inflate(R.layout.toast_layout, null, false);
+        TextView l1 = (TextView) layout.findViewById(R.id.toast_text);
+        LinearLayout linearLayout=(LinearLayout) layout.findViewById(R.id.toast_layout);
+        ImageView img=(ImageView) layout.findViewById(R.id.toast_icon);
+        l1.setText(message);
+        linearLayout.setBackgroundResource(R.drawable.bg_error);
+        img.setImageResource(R.drawable.ic_error_black_24dp);
+        toast.setView(layout);
+        toast.show();
+        return toast;
+    }
+
+    public static Toast info(Context context,String message,int duration){
+
+        Toast toast = new Toast(context);
+        toast.setDuration(duration);
+        View layout = LayoutInflater.from(context).inflate(R.layout.toast_layout, null, false);
+        TextView l1 = (TextView) layout.findViewById(R.id.toast_text);
+        LinearLayout linearLayout=(LinearLayout) layout.findViewById(R.id.toast_layout);
+        ImageView img=(ImageView) layout.findViewById(R.id.toast_icon);
+        l1.setText(message);
+        linearLayout.setBackgroundResource(R.drawable.bg_info);
+        img.setImageResource(R.drawable.ic_info_black_24dp);
+        toast.setView(layout);
+        toast.show();
+        return toast;
+    }
+
+    public static Toast warning(Context context,String message,int duration){
+
+        Toast toast = new Toast(context);
+        toast.setDuration(duration);
+        View layout = LayoutInflater.from(context).inflate(R.layout.toast_layout, null, false);
+        TextView l1 = (TextView) layout.findViewById(R.id.toast_text);
+        LinearLayout linearLayout=(LinearLayout) layout.findViewById(R.id.toast_layout);
+        ImageView img=(ImageView) layout.findViewById(R.id.toast_icon);
+        l1.setText(message);
+        linearLayout.setBackgroundResource(R.drawable.bg_warning);
+        img.setImageResource(R.drawable.ic_warning_black_24dp);
+        toast.setView(layout);
+        toast.show();
+        return toast;
+    }
+
+    public static Toast normal(Context context,String message,int duration){
+
+        Toast toast = Toast.makeText(context, message, duration);
         View view = toast.getView();
-        view.setBackgroundResource(R.drawable.bg_success);
         TextView text = (TextView) view.findViewById(android.R.id.message);
-        text.setTextColor(Color.WHITE);
-
-        toast.setText(text.getText());
-
-        toast.show();
-    }
-
-    public static void error(Context context, String message){
-
-        Toast toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
-        View view = toast.getView();
-        view.setBackgroundResource(R.drawable.bg_error);
-        TextView text = (TextView) view.findViewById(android.R.id.message);
-        text.setTextColor(Color.WHITE);
         toast.setText(text.getText());
         toast.show();
+
+        return toast;
     }
-
-    public static void info(Context context, String message){
-
-        Toast toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
-        View view = toast.getView();
-        view.setBackgroundResource(R.drawable.bg_info);
-        TextView text = (TextView) view.findViewById(android.R.id.message);
-        text.setTextColor(Color.WHITE);
-        toast.setText(text.getText());
-        toast.show();
-    }
-
-    public static void warning(Context context, String message){
-
-        Toast toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
-        View view = toast.getView();
-        view.setBackgroundResource(R.drawable.bg_warning);
-        TextView text = (TextView) view.findViewById(android.R.id.message);
-        text.setTextColor(Color.WHITE);
-        toast.setText(text.getText());
-        toast.show();
-    }
-
-    public static void normal(Context context, String message){
-
-        Toast toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
-        toast.show();
-    }
-
 }
